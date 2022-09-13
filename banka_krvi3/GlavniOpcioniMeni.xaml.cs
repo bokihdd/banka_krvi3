8e71452842cb8bbf9d46158c7ccb2984fae60448
nikolastrinic <99885789+nikolastrinic@users.noreply.github.com>
2022-09-13 13:06:25 +0200

    Funkcionalnosti login forme zavrseno. samo jos da se malo ulepsa

---
+++ banka_krvi3/GlavniOpcioniMeni.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace banka_krvi3
+{
+    /// <summary>
+    /// Interaction logic for GlavniOpcioniMeni.xaml
+    /// </summary>
+    public partial class GlavniOpcioniMeni : Window
+    {
+        public GlavniOpcioniMeni()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            var MainWindow = new MainWindow();
+            MainWindow.Show();
+        }
+    }
+}
--- banka_krvi3/MainWindow.xaml.cs
+++ banka_krvi3/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using MySql.Data.MySqlClient;
+
 namespace banka_krvi3
 {
     /// <summary>
@@ -24,6 +27,8 @@ namespace banka_krvi3
         {
             InitializeComponent();
         }
+
+        string MyConString = "SERVER=127.0.0.1;" + "DATABASE=bankakrvi;" + "UID=root;" + "PASSWORD=root03102000;";
 
         private void btnReg_Click(object sender, RoutedEventArgs e)
         {
@@ -43,6 +48,53 @@ namespace banka_krvi3
         private void Window_Closed(object sender, EventArgs e)
         {
             System.Windows.Application.Current.Shutdown();
+        }
+
+        private void btnUlogujSe_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtIme.Text.Length == 0 || txtSifra.Text.Length == 0)
+            {
+                MessageBox.Show("Popunite sva polja!");
+            }
+            else {
+
+                string username = txtIme.Text;
+                string password = txtSifra.Text;
+
+                try
+                {
+                    MySqlConnection connection = new MySqlConnection(MyConString);
+                    connection.Open();                  
+                    string query = "select * from korisnik where KorisnickoIme = '" + txtIme.Text + "' and Sifra = '" + txtSifra.Text + "'";
+                    MySqlDataAdapter sda = new MySqlDataAdapter(query, connection);
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        username = txtIme.Text;
+                        password = txtSifra.Text;
+
+                        GlavniOpcioniMeni prozor = new GlavniOpcioniMeni();
+                        this.Visibility = Visibility.Hidden;
+                        prozor.Show();
+                    }
+                    else {
+                        MessageBox.Show("Pogresno korisnicko ime ili sifra!");
+                    }
+                    
+
+                    connection.Close();
+                }
+                catch
+                {
+                    MessageBox.Show("Error!");
+                }
+                
+                    
+                    
+
+            }
         }
     }
 }
