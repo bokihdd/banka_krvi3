2cce61c0a3ed014104cd7c90c232fceaf317e328
nikolastrinic <99885789+nikolastrinic@users.noreply.github.com>
2022-09-13 12:24:55 +0200

    Gotova lista radnika

--- banka_krvi3/ListaRadnika.xaml.cs
+++ banka_krvi3/ListaRadnika.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using MySql.Data.MySqlClient;
+
 namespace banka_krvi3
 {
     /// <summary>
@@ -22,6 +26,54 @@ namespace banka_krvi3
         public ListaRadnika()
         {
             InitializeComponent();
+        }
+
+        string MyConString = "SERVER=127.0.0.1;" + "DATABASE=bankakrvi;" + "UID=root;" + "PASSWORD=root03102000;";
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string query = "select * from korisnik";
+            MySqlConnection connection = new MySqlConnection(MyConString);
+            MySqlCommand cmdSel = new MySqlCommand(query, connection);
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(cmdSel);
+            da.Fill(dt);
+            ListaRadnikaDataGrid.DataContext = dt;
+        }
+
+        private void ListaRadnikaPretraziButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListaRadnikaImeTextBox.Text.Length == 0 || ListaRadnikaPrezimeTextBox.Text.Length == 0)
+            {
+                MessageBox.Show("Popunite sva polja!");
+            }
+            else
+            {
+                string query = "select * from korisnik where Ime = '" + ListaRadnikaImeTextBox.Text + "' and Prezime = '" + ListaRadnikaPrezimeTextBox.Text + "'";
+                MySqlConnection connection = new MySqlConnection(MyConString);
+                MySqlCommand cmdSel = new MySqlCommand(query, connection);
+                DataTable dt = new DataTable();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmdSel);
+                da.Fill(dt);
+                ListaRadnikaDataGrid.DataContext = dt;
+            }
+        }
+
+        private void ListaRadnikaOsveziButton_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select * from korisnik";
+            MySqlConnection connection = new MySqlConnection(MyConString);
+            MySqlCommand cmdSel = new MySqlCommand(query, connection);
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(cmdSel);
+            da.Fill(dt);
+            ListaRadnikaDataGrid.DataContext = dt;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            var MainWindow = new MainWindow();
+            MainWindow.Show();
         }
     }
 }
