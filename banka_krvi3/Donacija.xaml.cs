8e0c1b0fd8b511994065bfd3a79b0dfbf7ec039f
nikolastrinic <99885789+nikolastrinic@users.noreply.github.com>
2022-09-14 17:44:46 +0200

    donacija krvi gotovo

--- banka_krvi3/Donacija.xaml.cs
+++ banka_krvi3/Donacija.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,6 +25,11 @@ namespace banka_krvi3
     {
         string kljucD;
         string kljucP;
+        string krvGDonor;
+        string krvGPacijent;
+        DateTime now = DateTime.Now;
+
+        
         public Donacija()
         {
             InitializeComponent();
@@ -76,42 +82,147 @@ namespace banka_krvi3
             connection.Open();
             MySqlCommand cmdSel = new MySqlCommand(query1, connection);
             MySqlDataReader dr = cmdSel.ExecuteReader();
-           var queryResult = cmdSel.ExecuteReader();                                                                         //DONOR
+            dr.Close();
+
+            var queryResult = cmdSel.ExecuteReader();                                                                         //id DONOR
             queryResult.Read();
             kljucD=queryResult.GetString(0);
-            MessageBox.Show(kljucD);
+            
             queryResult.Close();
 
             string query2= "SELECT idPacijenta FROM pacijent WHERE pacijent.Ime = '" + cbPrezimeP.Text + "'";
             MySqlCommand cmdSel2 = new MySqlCommand(query2, connection);
             var queryResult2 = cmdSel2.ExecuteReader();
-            queryResult2.Read();                                                                                             //pacijent
+            queryResult2.Read();                                                                                             // id pacijent
             kljucP = queryResult2.GetString(0);
-            MessageBox.Show(kljucP);
+            
             queryResult2.Close();
 
-
-
+            string query3 = "SELECT KrvnaGrupa FROM donor WHERE donor.Ime = '" + cbImeD.Text + "'";
+            MySqlCommand cmdSel3 = new MySqlCommand(query3, connection);
+            var queryResult3 = cmdSel3.ExecuteReader();
+            queryResult3.Read();                                                                                             // krvna grupa donora
+            krvGDonor = queryResult3.GetString(0);
             
+            queryResult3.Close();
 
+            string query4 = "SELECT KrvnaGrupa FROM pacijent WHERE pacijent.Ime = '" + cbPrezimeP.Text + "'";
+            MySqlCommand cmdSel4 = new MySqlCommand(query4, connection);
+            var queryResult4 = cmdSel4.ExecuteReader();
+            queryResult4.Read();                                                                                             // krvna grupa pacijent
+            krvGPacijent = queryResult4.GetString(0);
             
+            queryResult4.Close();
 
+            if (krvGDonor == "A+")
+            {
+                if (krvGPacijent == "A+" || krvGPacijent == "AB+")
+                {
 
+                    string query = "insert into donacija (Donor, Pacijent, DatumPreuzimanja) values ('" + kljucD + "', '" + kljucP + "', '" + now + "')";
+                    cmdSel = new MySqlCommand(query, connection);
+                    var queryResult1 = cmdSel.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("Krvne grupe nisu kompatibilne!");
+                }
+            }
+            else if (krvGDonor == "O+")
+            {
+                if (krvGPacijent == "O+" || krvGPacijent == "A+" || krvGPacijent == "B+" || krvGPacijent == "AB+")
+                {
+                    string query = "insert into donacija (Donor, Pacijent, DatumPreuzimanja) values ('" + kljucD + "', '" + kljucP + "', '" + now + "')";
+                    cmdSel = new MySqlCommand(query, connection);
+                    var queryResult1 = cmdSel.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("Krvne grupe nisu kompatibilne!");
+                }
+            }
+            else if (krvGDonor == "B+")
+            {
+                if (krvGPacijent == "B+" || krvGPacijent == "AB+")
+                {
+                    string query = "insert into donacija (Donor, Pacijent, DatumPreuzimanja) values ('" + kljucD + "', '" + kljucP + "', '" + now + "')";
+                    cmdSel = new MySqlCommand(query, connection);
+                    var queryResult1 = cmdSel.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("Krvne grupe nisu kompatibilne!");
+                }
+            }
+            else if (krvGDonor == "AB+")
+            {
+                if (krvGPacijent == "AB+")
+                {
+                    string query = "insert into donacija (Donor, Pacijent, DatumPreuzimanja) values ('" + kljucD + "', '" + kljucP + "', '" + now + "')";
+                    cmdSel = new MySqlCommand(query, connection);
+                    var queryResult1 = cmdSel.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("Krvne grupe nisu kompatibilne!");
+                }
 
+            }
+            else if (krvGDonor == "A-")
+            {
+                if (krvGPacijent == "A+" || krvGPacijent == "A-" || krvGPacijent == "AB+" || krvGPacijent == "AB-")
+                {
+                    string query = "insert into donacija (Donor, Pacijent, DatumPreuzimanja) values ('" + kljucD + "', '" + kljucP + "', '" + now + "')";
+                    cmdSel = new MySqlCommand(query, connection);
+                    var queryResult1 = cmdSel.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("Krvne grupe nisu kompatibilne!");
+                }
 
+            }
+            else if (krvGDonor == "O-")
+            {          
+                    string query = "insert into donacija (Donor, Pacijent, DatumPreuzimanja) values ('" + kljucD + "', '" + kljucP + "', '" + now + "')";
+                cmdSel = new MySqlCommand(query, connection);
+                var queryResult1 = cmdSel.ExecuteNonQuery();
+            }
+            else if (krvGDonor == "B-")
+            {
+                if (krvGPacijent == "B+" || krvGPacijent == "B-" || krvGPacijent == "AB+" || krvGPacijent == "AB-")
+                {
+                    string query = "insert into donacija (Donor, Pacijent, DatumPreuzimanja) values ('" + kljucD + "', '" + kljucP + "', '" + now + "')";
+                    cmdSel = new MySqlCommand(query, connection);
+                    var queryResult1 = cmdSel.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("Krvne grupe nisu kompatibilne!");
+                }
 
+            }
+            else if (krvGDonor == "AB-")
+            {
+                if (krvGPacijent == "AB+" || krvGPacijent == "AB-")
+                {
+                    string query = "insert into donacija (Donor, Pacijent, DatumPreuzimanja) values ('" + kljucD + "', '" + kljucP + "', '" + now + "')";
+                    cmdSel = new MySqlCommand(query, connection);
+                    var queryResult1 = cmdSel.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("Krvne grupe nisu kompatibilne!");
+                }
 
+            }
+            MessageBox.Show("Uspesno donirana krv!");
+        }
 
-
-
-
-
-
-
-
-
-
-
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            var GlavniOpcioniMeni = new GlavniOpcioniMeni();
+            GlavniOpcioniMeni.Show();
         }
     }
   }
